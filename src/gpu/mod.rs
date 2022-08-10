38ad140b272f159b712cef78de394fa039b9a753
dtlnor <dtlnor@hotmail.com>
2022-08-10 17:30:39 +0900

    Update mod.rs

--- src/gpu/mod.rs
+++ src/gpu/mod.rs
@@ -20,13 +20,7 @@ struct Job {
 }
 
 fn gpu_thread(receiver: Receiver<Job>) {
-    #[cfg(target_family = "unix")]
-    use glutin::platform::unix::EventLoopExtUnix;
-    #[cfg(target_family = "windows")]
-    use glutin::platform::windows::EventLoopExtWindows;
-
-    let event_loop: glutin::event_loop::EventLoop<()> =
-        glutin::event_loop::EventLoop::new_any_thread();
+    let event_loop: glutin::event_loop::EventLoop<()> = glutin::event_loop::EventLoop::new();
     let cb = glutin::ContextBuilder::new()
         .with_gl(glutin::GlRequest::Specific(glutin::Api::OpenGl, (3, 3)))
         .with_gl_profile(glutin::GlProfile::Core);
