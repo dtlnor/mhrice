1e969d1020fed9d89030fad3ed5c31d39cfdbb07
dtlnor <dtlnor@hotmail.com>
2022-08-31 16:06:16 +0900

    support NS files

--- src/pak.rs
+++ src/pak.rs
@@ -101,6 +101,8 @@ impl<F: Read + Seek> PakReader<F> {
                         let power = base.modpow(&exponent, &modulus);
                         let key_vec = power.to_bytes_le();
                         if key_vec.len() > 32 {
+                            eprintln!(" base {}\n modulus {}\n exponent {}\n power {}\n flag {}\n", base, modulus, exponent, power, flag);
+
                             bail!("Key too long")
                         }
                         let mut key = [0; 32];
@@ -159,25 +161,36 @@ impl<F: Read + Seek> PakReader<F> {
             .context("Unknown extension")?;
         for suffix in suffix.iter().rev() {
             let full_path = format!("natives/STM/{}.{}", path, suffix);
+            let full_path_ns = format!("natives/NSW/{}.{}", path, suffix);
             let full_path_x64 = format!("{}.x64", &full_path);
             let full_path_stm = format!("{}.STM", &full_path);
+            let full_path_nsw = format!("{}.NSW", &full_path);
 
             let mut result = vec![];
 
             for &language in LANGUAGE_LIST {
-                let (path_l, path_x64_l, path_stm_l) = if language.is_empty() {
+                let (path_l, path_ns_l, path_x64_l, path_stm_l, path_nsw_l) = if language.is_empty() {
                     (
                         full_path.clone(),
+                        full_path_ns.clone(),
                         full_path_x64.clone(),
                         full_path_stm.clone(),
+                        full_path_nsw.clone(),
                     )
                 } else {
                     let path_l = format!("{}.{}", &full_path, language);
+                    let path_ns_l = format!("{}.{}", &full_path_ns, language);
                     let path_x64_l = format!("{}.{}", &full_path_x64, language);
                     let path_stm_l = format!("{}.{}", &full_path_stm, language);
-                    (path_l, path_x64_l, path_stm_l)
+                    let path_nsw_l = format!("{}.{}", &full_path_nsw, language);
+                    (path_l, path_ns_l, path_x64_l, path_stm_l, path_nsw_l)
                 };
                 if let Some(index) = self.find_file_internal(path_l) {
+                    result.push(I18nPakFileIndex { language, index });
+                    continue;
+                }
+
+                if let Some(index) = self.find_file_internal(path_ns_l) {
                     result.push(I18nPakFileIndex { language, index });
                     continue;
                 }
@@ -188,6 +201,11 @@ impl<F: Read + Seek> PakReader<F> {
                 }
 
                 if let Some(index) = self.find_file_internal(path_stm_l) {
+                    result.push(I18nPakFileIndex { language, index });
+                    continue;
+                }
+
+                if let Some(index) = self.find_file_internal(path_nsw_l) {
                     result.push(I18nPakFileIndex { language, index });
                     continue;
                 }
@@ -271,7 +289,26 @@ impl<F: Read + Seek> PakReader<F> {
                 }
                 Ok(decoded)
             }
-            _ => bail!("Unsupported format: {}", format),
+            _ => {
+                if (format & 1) == 1{
+                    let mut decompressed = Vec::new();
+                    flate::Decoder::new(&data[..]).read_to_end(&mut decompressed)?;
+                    if u64::try_from(decompressed.len()).unwrap() != len {
+                        bail!("Expected size {}, actual size {}", len, decompressed.len());
+                    }
+                    Ok(decompressed)
+
+                }else if (format & 2) == 2{
+                    let decoded = zstd::decode_all(&data[..])?;
+                    if u64::try_from(decoded.len()).unwrap() != len {
+                        bail!("Expected size {}, actual size {}", len, decoded.len());
+                    }
+                    Ok(decoded)
+
+                }else{
+                    bail!("Unsupported format: {}", format);
+                }
+            }
         }
     }
 
