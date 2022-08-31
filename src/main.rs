5a2f5068d1af79a6252269037e1ca14a5b60a5e3
dtlnor <dtlnor@hotmail.com>
2022-08-31 16:07:38 +0900

    disable gpu init

--- src/main.rs
+++ src/main.rs
@@ -1308,7 +1308,9 @@ fn map(pak: Vec<String>, name: String, scale: String, tex: String, output: Strin
 }
 
 fn main() -> Result<()> {
-    gpu::gpu_init();
+    // disable this as I don't have a display on my wsl,
+    // and don't want to download too many package
+    // gpu::gpu_init();
     match Mhrice::from_args() {
         Mhrice::Dump { pak, name, output } => dump(pak, name, output),
         Mhrice::DumpIndex {
